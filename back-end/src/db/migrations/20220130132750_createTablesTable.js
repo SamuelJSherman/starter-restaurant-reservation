d89ad84dd742a7303fccfbc6a2c9e5e22e92ddae
SamuelJSherman <samueljessesherman@gmail.com>
2022-01-30 13:49:56 -0800

    fixed seeds

--- back-end/src/db/migrations/20220130132750_createTablesTable.js
+++ back-end/src/db/migrations/20220130132750_createTablesTable.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
 		table.integer("capacity").notNullable();
 		table.string("status").notNullable();
 		
-		// let's make a foreign key here:
+		//foreign key here:
 		table.integer("reservation_id").unsigned();
 		table.foreign("reservation_id")
 			.references("reservation_id")
